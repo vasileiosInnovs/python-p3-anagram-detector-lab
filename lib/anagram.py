63a0d25c65a8c716a16700f50da6b52aa08c30d6
Hagee <hagee.ktr@outlook.com>
2025-05-09 17:10:00 +0300

    Generate code that returns an anagram upon entry

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+words = ['google', 'inlets', 'banana']
+
+class Anagram:
+    def __init__(self, target):
+        self.target = target
+
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word) == sorted(self.target)]
+            
+listen = Anagram("listen")
+print(listen.match(words))
